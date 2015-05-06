700437b32874f52dc47edb58a60919421029f279
rsearls <rsearls@redhat.com>
2015-05-06 11:45:17 -0400

    wise-gwt relocated

--- wise-gwt/src/main/java/org/jboss/wise/client/event/CancelledEventHandler.java
+++
@@ -1,7 +0,0 @@
-package org.jboss.wise.client.event;
-
-import com.google.gwt.event.shared.EventHandler;
-
-public interface CancelledEventHandler extends EventHandler {
-  void onCancelled(CancelledEvent event);
-}
